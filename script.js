d7dac4daf33444332862fcae87908bb47c09efda
Ajay Singh <ajay04132004@gmail.com>
2025-06-27 00:55:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,8 @@ function showTab(id) {
   document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 }
+
+// Toggle mobile nav
+function toggleMenu() {
+  document.querySelector('.nav-links').classList.toggle('show');
+}
